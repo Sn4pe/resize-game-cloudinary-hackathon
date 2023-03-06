ea0d5de21b40d4a81c2474dcc3442611b8633a83
Sn4pe <hi@sn4pe.com>
2023-03-06 16:02:06 +0100

    chore: eliminar comentarios

--- src/components/store.ts
+++ src/components/store.ts
@@ -1,9 +1,9 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
-const browserLanguage = navigator.language.substr(0,2); // Obtiene el código de idioma del navegador
+const browserLanguage = navigator.language.substr(0,2);
 
-const defaultLanguage = browserLanguage === 'en' || browserLanguage === 'es' ? browserLanguage : 'en'; // Configura el idioma predefinido a "en" si no es "en" ni "es"
+const defaultLanguage = browserLanguage === 'en' || browserLanguage === 'es' ? browserLanguage : 'en'; 
 
 const languageStore: Writable<string> = writable(defaultLanguage);
 
