9f036f29b1fea248427ff108a73a564a65b9cf0f
Sn4pe <hi@sn4pe.com>
2023-03-06 16:00:48 +0100

    fix: :bug: error con idiomas

--- src/components/store.ts
+++ src/components/store.ts
@@ -1,14 +1,20 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
-const defaultLanguage = navigator.language.substr(0, 2) || 'en';
+const browserLanguage = navigator.language.substr(0,2); // Obtiene el código de idioma del navegador
+
+const defaultLanguage = browserLanguage === 'en' || browserLanguage === 'es' ? browserLanguage : 'en'; // Configura el idioma predefinido a "en" si no es "en" ni "es"
 
 const languageStore: Writable<string> = writable(defaultLanguage);
 
 const lang = {
   subscribe: languageStore.subscribe,
   set: (language: string) => {
-    languageStore.set(language);
+    if(language === 'en' || language === 'es') {
+      languageStore.set(language);
+    } else {
+      languageStore.set('en');
+    }
   },
   current: defaultLanguage, 
 };
